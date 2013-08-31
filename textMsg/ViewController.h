d8eceefff413219cf9fb3fb00fc98478af1f3f0e
rlove1221 <richard.morena@me.com>
2013-08-31 11:48:52 -0400

    add source

---
+++ textMsg/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  textMsg
+//
+//  Created by Richard Morena on 8/31/13.
+//  Copyright (c) 2013 Richard Morena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+@interface ViewController : UIViewController<MFMessageComposeViewControllerDelegate>
+
+@end
